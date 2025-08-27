2d5d940cb7eb535f984fa25ea5e654f641c93299
Datierras <datierrasyregdom@gmail.com>
2025-08-27 11:36:32 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+// Firebase config (tu proyecto)
+export const firebaseConfig = {
+  apiKey: "AIzaSyAHXCfXoJK1p_naZf5v0_cAa6cphX1e1E8",
+  authDomain: "exptcoord.firebaseapp.com",
+  projectId: "exptcoord",
+  storageBucket: "exptcoord.firebasestorage.app",
+  messagingSenderId: "416639039117",
+  appId: "1:416639039117:web:d9422f6d853a760a3014c4",
+  measurementId: "G-94PRRLFZV4"
+};
