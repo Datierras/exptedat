7e5677e840121f5381060e9911ac896e9e1dab68
Datierras <datierrasyregdom@gmail.com>
2025-08-28 11:24:24 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,9 @@
-// config.js
-// Esta es la configuración de tu proyecto de Firebase.
-
-export const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyAHXCfXoJK1p_naZf5v0_cAa6cphX1e1E8",
   authDomain: "exptcoord.firebaseapp.com",
   projectId: "exptcoord",
-  storageBucket: "exptcoord.appspot.com", // Corregí el dominio a .appspot.com, que es el más común.
+  storageBucket: "exptcoord.appspot.com"
   messagingSenderId: "416639039117",
   appId: "1:416639039117:web:d9422f6d853a760a3014c4",
 };
-
-// Inicializar Firebase (necesario para el script app.js corregido)
-firebase.initializeApp(firebaseConfig);
+  
