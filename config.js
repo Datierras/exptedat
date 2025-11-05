591687de56f9c2b8fdcb0b5ccd8f97fe4e7862d6
Datierras <datierrasyregdom@gmail.com>
2025-11-05 12:36:14 -0300

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-// config.js
-const firebaseConfig = {
-  apiKey: "AIzaSyAHXCfXoJK1p_naZf5v0_cAa6cphX1e1E8",
-  authDomain: "exptcoord.firebaseapp.com",
-  projectId: "exptcoord",
-  storageBucket: "exptcoord.appspot.com",
-  messagingSenderId: "416639039117",
-  appId: "1:416639039117:web:d9422f6d853a760a3014c4"
-};
